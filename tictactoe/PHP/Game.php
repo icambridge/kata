7afcf86c1673d8218946cf4ca1c0eef0964c0ca3
Iain Cambridge <github@icambridge.me>
2011-08-27 19:37:37 +0100

    Added Game::place()

--- tictactoe/PHP/Game.php
+++ tictactoe/PHP/Game.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once "Exceptions.php";
+
 /**
  * 
  *
@@ -19,6 +21,43 @@ class Game
 
     public function getLayout()
     {
+        return $this->layout;
+    }
+
+    public function place($type, $column, $row)
+    {
+        $type = strtolower($type);
+        if ($type != "knot" && $type != "cross") {
+            throw new InvalidMove("'".$type."' isn't a valid type");
+        }
+
+        if (!is_int($column) && !ctype_digit($column)) {
+            throw new InvalidMove("Column isn't a number.");
+        }
+
+        $column = (int) $column;
+        if ($column < 1 || $column > 3) {
+            throw new InvalidMove("'".$column."' isn't a valid number for the column, expected between 1 and 3");
+        }
+
+        if (!is_int($row) && !ctype_digit($row)) {
+            throw new InvalidMove("Row isn't a valid number.");
+        }
+
+        $row = (int) $row;
+        if ($row < 1 || $row > 3) {
+            throw new InvalidMove("'".$row."' isn't a valid number for the row, expected between 1 and 3");
+        }
+
+        $row--;
+        $column--;
+
+        if ($this->layout[$column][$row] !== NULL) {
+            throw new InvalidMove("Column:".$column.", Row:".$row." isn't empty");
+        }
+
+        $this->layout[$column][$row] = $type;
+
         return $this->layout;
     }
 }
--- tictactoe/PHP/tests/GameTest.php
+++ tictactoe/PHP/tests/GameTest.php
@@ -16,5 +16,21 @@ class GameTest extends PHPUnit_Framework_TestCase
             $this->assertEquals(3, sizeof($layout[$i]), "The layout column ".$i." doesn't have three high");
         }
     }
+
+    public function testInsertKnot()
+    {
+        $game = new Game();
+        $layout = $game->place('knot',3,1);
+        $this->assertTrue(is_array($layout), "The layout isn't an array");
+        $this->assertTrue(isset($layout[2][0]), "The bottom right hand corner isn't set");
+        $this->assertEquals('knot',$layout[2][0], "The bottom right hand corner doesn't have a knot in it");
+    }
+
+    public function testInvalidType()
+    {
+        $this->setExpectedException("InvalidMove", "'fake' isn't a valid type");
+        $game = new Game();
+        $game->place("fake",1,1);
+    }
 }
  
