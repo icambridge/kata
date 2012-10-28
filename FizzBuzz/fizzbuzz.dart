b8c411fad3475259d2fe0cce7e7bc0cf61b5a832
Iain Cambridge <github@icambridge.me>
2012-10-28 21:23:12 +0000

    Added fizzbuzz in Dart

---
+++ FizzBuzz/fizzbuzz.dart
@@ -0,0 +1,17 @@
+
+main() {
+
+    for (int i = 0; i < 100; i++) {
+        if ((i % 15) == 0) {
+            print('FizzBuzz');
+        } else if ((i % 5) == 0) {
+            print('Buzz');
+        } else if ((i % 3) == 0) {
+            print('Fizz');
+        } else {
+            print(i);
+        }
+
+    }
+
+}
